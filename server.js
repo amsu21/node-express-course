825dfbc74c99101006828a4502b8b18812fd5af7
Warner <a.s.warner@Warners-MacBook-Pro.local>
2021-10-09 00:38:01 -0400

    Set up the express server

---
+++ server.js
@@ -0,0 +1,9 @@
+// ACCESS EXPRESS LIBRARY
+const express = require('express');
+const app = express();
+
+
+// LISTENER LINE
+app.listen(8000, function() {
+    console.log("Server is runnin!!!");
+})
