15befd6078970a9e29126c5ec75dbaeb9a46cb0d
Warner <a.s.warner@Warners-MacBook-Pro.local>
2021-10-09 00:54:44 -0400

    Added login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // ACCESS EXPRESS LIBRARY
 const express = require('express');
 const app = express();
+// ACCESS BODY PARSER
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 
 // MOCK USER DATA
@@ -9,6 +12,7 @@ const mockUserData=[
     {name:'Baasir'}
 ]
 
+// GET ROUTE
 app.get('/users', function(req, res){
     res.json({
         success: true,
@@ -24,6 +28,29 @@ app.get('/users/:id', function(req, res){
         message: 'got a user!!!!!!',
         user: req.params.id
     })
+})
+
+// POST ROUTE
+app.post('/login', function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUserName="amsuTheGOAT";
+    const mockPassword="superSecret";
+
+    // IF USERNAME AND PASSWORD MATCH THEN DO THE FOLLOWING JSON DATA
+    if (username === mockUserName && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username matches!!!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username dont match brooooooo!'
+        })
+    }
 })
 
 // LISTENER LINE
