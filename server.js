4920d0123c3d08e4a8cbbd560ce60771da394201
Warner <a.s.warner@Warners-MacBook-Pro.local>
2021-10-09 00:42:53 -0400

    Add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 const app = express();
 
 
+// MOCK USER DATA
+const mockUserData=[
+    {name:'Amsu'},
+    {name:'Baasir'}
+]
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfully got some users. Good job!',
+        users: mockUserData
+    })
+})
+
 // LISTENER LINE
 app.listen(8000, function() {
     console.log("Server is runnin!!!");
